646239591b3ce2d015571bc5346fa4b6d684d033
JD Riviere <tilithar@gmail.com>
2017-02-06 23:44:13 -0700

    Added s3css.js file in JS folder. Removed all other unnecessary files.

---
+++ js/s3css.js
@@ -0,0 +1,4 @@
+$(document).ready (function() {
+    // This is where all the S3CSS functions are implemented. Most of them will
+    // serve to fix problems that occur within the CSS-only framework.
+});
