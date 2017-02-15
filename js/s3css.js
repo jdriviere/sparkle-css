04a7ade6863ecbd5146e04bf6bff0eb495ab4ec7
JD Riviere <tilithar@gmail.com>
2017-02-15 01:22:22 -0700

    Removed all CSS files to increase merging compatibility.

--- js/s3css.js
+++ js/s3css.js
@@ -1,4 +1,31 @@
+// ==========================================================
+// S3CSS.js FRAMEWORK
+// ==========================================================
 $(document).ready (function() {
-    // This is where all the S3CSS functions are implemented. Most of them will
-    // serve to fix problems that occur within the CSS-only framework.
+	// This is where all the S3CSS functions are implemented. Most of them will
+	// serve to fix problems that occur within the CSS-only framework. This function
+	// will load all the S3CSS framework functions.
+	navDrop();
 });
+
+// -----------------------------
+// FUNCTIONS
+// -----------------------------
+// This function setups up the dropdown menu functionality.
+function navDrop() {
+	$('nav ul li > a:not(:only-child)').click (function (e) {
+		$(this).siblings('.nav-dropdown').toggle();
+		// Prevents multiple dropdowns to remain open at the same time.
+		$('.nav-dropdown').not($(this).siblings()).hide();
+		e.stopPropagation();
+	});
+	
+	// Hides dropdown when user clicks anywhere within the HTML page.
+	$('html').click(function() {
+		$('.nav-dropdown').hide();
+	});
+} // End of NavDrop
+
+// This function fixes navigation by forcing the mobile navigation to disappear
+// when the navigation bar reaches Desktop size, when it is already opened.
+function navResizeFix () {} // End of NavResizeFix
