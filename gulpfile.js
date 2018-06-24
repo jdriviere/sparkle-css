1baa3a7a2a81606156372488cb0023bc28f7d686
JD Riviere <tilithar@gmail.com>
2018-06-24 13:51:12 -0600

    Code cleanup. Packages updated. Gulpfile updated.

--- gulpfile.js
+++ gulpfile.js
@@ -15,43 +15,56 @@ var imageminJPG = require('imagemin-jpegtran');
 var imageminSVG = require('imagemin-svgo');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 var browserSync = require('browser-sync').create();
 
 /* ==================================== */
 /* TASKS
 /* ==================================== */
 // RENDER CSS FILES
-gulp.task('sassify', function() {
+gulp.task('sassify', function () {
+    var browser_pref = [
+        autoprefixer({
+            browsers: ['last 2 versions']
+        })
+    ];
+    var shrink_opt = [cssnano()];
+
     return gulp.src('src/sass/sparkle.sass')
-        .pipe(sass({ outputStyle: 'expanded' })
+        .pipe(sass({
+                outputStyle: 'expanded'
+            })
             .on('error', sass.logError)
         )
+        .pipe(postcss(browser_pref))
         .pipe(rename('sparkle.css'))
-		.pipe(gulp.dest('dist/css/'))
-		.pipe(cleanCSS())
-		.pipe(rename('sparkle.min.css'))
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(postcss(shrink_opt))
+        .pipe(rename('sparkle.min.css'))
         .pipe(gulp.dest('dist/css/'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('watch:sass', function() {
+gulp.task('watch:sass', function () {
     gulp.watch('src/scss/**/*.sass', ['sassify'], browserSync.reload);
     console.log('Watching CSS files.');
 });
 
 // RENDER JS FILES
-gulp.task('uglify', function(cb) {
+gulp.task('uglify', function (cb) {
     pump([
-        gulp.src('src/js/sparkle.js'),
-        gulp.dest('dist/js/'),
-        uglify(),
-        rename('sparkle.min.js'),
-        gulp.dest('dist/js/')
-    ],
-    cb);
+            gulp.src('src/js/sparkle.js'),
+            gulp.dest('dist/js/'),
+            uglify(),
+            rename('sparkle.min.js'),
+            gulp.dest('dist/js/')
+        ],
+        cb);
 });
 
-gulp.task('watch:js', function() {
+gulp.task('watch:js', function () {
     gulp.watch('src/js/*.js', ['uglify'], browserSync.reload);
     console.log('Watching JS files.');
 });
@@ -60,30 +73,42 @@ gulp.task('watch:js', function() {
 gulp.task('pug', function buildHTML() {
     return gulp.src('dev/*.pug')
         .pipe(
-            pug({ pretty: true })
+            pug({
+                pretty: true
+            })
         )
         .pipe(gulp.dest('./'))
-        .pipe(browserSync.reload({ stream: true })); // <-- Remove if it doesn't work
+        .pipe(browserSync.reload({
+            stream: true
+        })); // <-- Remove if it doesn't work
 });
 
-gulp.task('watch:html', function() {
+gulp.task('watch:html', function () {
     gulp.watch('dev/**/*.pug', ['pug'], browserSync.reload);
     console.log('Watching HTML files.');
 });
 
 // RENDER IMAGE FILES
-gulp.task('minimg', function() {
+gulp.task('minimg', function () {
     return gulp.src(['src/img/*.jpg', 'src/img/*.png', 'src/img/*.svg'])
         .pipe(imagemin(
-            imagemin.jpegtran({ progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo({ plugins: [{removeViewBox: true}] })
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            imagemin.optipng({
+                optimizationLevel: 5
+            }),
+            imagemin.svgo({
+                plugins: [{
+                    removeViewBox: true
+                }]
+            })
         ))
         .pipe(gulp.dest('dist/img/'));
 });
 
 // CREATE SERVER
-gulp.task('serve', ['watch:html', 'watch:sass', 'watch:js'], function() {
+gulp.task('serve', ['watch:html', 'watch:sass', 'watch:js'], function () {
     browserSync.init({
         server: "./"
     });
