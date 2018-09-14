945c1448bb43ae32b036b45ff803d5708861f56b
J. Djimitry Riviere <tilithar@gmail.com>
2018-09-14 08:17:25 -0600

    chore(gulpfile): Added IE 9 support and bumped version support to last 3.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ const cssnano = require('cssnano');
 gulp.task('sassify', function() {
   var browser_pref = [
     autoprefixer({
-      browsers: ['last 2 versions']
+      browsers: ['last 3 versions', "IE 9"]
     })
   ];
   var shrink_opt = [cssnano()];
@@ -58,74 +58,3 @@ gulp.task('pug', function buildHTML() {
 
 // DEFAULT RUN
 gulp.task('default', ['pug', 'sassify']);
-
-// =================================
-// DEPRECATED MODULES
-// =================================
-// const pump = require('pump');
-// const uglify = require('gulp-uglify'); // Only deactivated for now
-// const concatCSS = require('gulp-concat-css');
-// const cleanCSS = require('gulp-clean-css');
-// const imagemin = require('gulp-imagemin');
-// const browserSync = require('browser-sync').create();
-
-// =================================
-// DEPRECATED TASKS
-// =================================
-// RENDER JS FILES
-// gulp.task('uglify', function (cb) {
-//   pump([
-//       gulp.src('src/js/sparkle.js'),
-//       gulp.dest('dist/js/'),
-//       uglify(),
-//       rename('sparkle.min.js'),
-//       gulp.dest('dist/js/')
-//     ],
-//     cb);
-// });
-
-// gulp.task('watch:js', function() {
-//   gulp.watch('src/js/*.js', ['uglify'], browserSync.reload);
-//   console.log('Watching JS files.');
-// });
-
-// RENDER IMAGE FILES
-// gulp.task('minimg', function() {
-//   return gulp.src(['src/img/*.jpg', 'src/img/*.png', 'src/img/*.svg'])
-//     .pipe(imagemin(
-//       imagemin.jpegtran({
-//         progressive: true
-//       }),
-//       imagemin.optipng({
-//         optimizationLevel: 5
-//       }),
-//       imagemin.svgo({
-//         plugins: [{
-//           removeViewBox: true
-//         }]
-//       })
-//     ))
-//     .pipe(gulp.dest('dist/img/'));
-// });
-
-// WATCHES
-// gulp.task('watch:sass', function() {
-//   gulp.watch('src/sass/**/*.sass', ['sassify'], browserSync.reload);
-//   console.log('Watching CSS files.');
-// });
-
-// gulp.task('watch:html', function() {
-//   gulp.watch('dev/**/*.pug', ['pug'], browserSync.reload);
-//   console.log('Watching HTML files.');
-// });
-
-// CREATE SERVER
-// gulp.task('serve', ['watch:html', 'watch:sass', 'watch:js'], function() {
-//   browserSync.init({
-//     server: "./"
-//   });
-
-//   gulp.watch(['src/sass/sparkle.sass'], ['sassify']);
-//   gulp.watch(['dev/**/*.pug'], ['pug']);
-//   gulp.watch("./index.html").on('change', browserSync.reload);
-// });
