828ec58398defa78103210d2a9fce67feb570b7d
J. Djimitry Riviere <tilithar@gmail.com>
2018-09-15 12:20:01 -0600

    docs(sparkle.css): Header added in cleaner fashion by using postcss-header.

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,34 @@ const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const header = require('postcss-header');
 const cssnano = require('cssnano');
+
+const pkg = require('./package.json');
 
 /* ==================================== */
 /* TASKS
 /* ==================================== */
 // RENDER CSS FILES
 gulp.task('sassify', function() {
-  const browser_pref = [
+  const banner = `
+  /*!
+    * @name         ${pkg.name}
+    * @author       ${pkg.author}
+    * @version      ${pkg.version} - released on 09/01/2018
+    * @website      ${pkg.homepage}
+    * Welcome and thank you for using Sparkle, a lightweight, open-source CSS framework
+    * to help you create simple, responsive website templates. It was developed by J. Djimitry Riviere
+    * in late 2016, and continues to be improved upon by its developer to bring more creative features.
+    * The major key to this framework lies within its ease of us to deliver superb results
+    * when creating a website.
+    * Thank you for using Sparkle! Happy Designing!
+    */
+  `;
+  const options = [
+    header({
+      header: banner
+    }),
     autoprefixer({
       browsers: ['last 3 versions', "IE 9"]
     })
@@ -29,7 +49,7 @@ gulp.task('sassify', function() {
       })
       .on('error', sass.logError)
     )
-    .pipe(postcss(browser_pref))
+    .pipe(postcss(options))
     .pipe(rename('sparkle.css'))
     .pipe(gulp.dest('dist/css/'))
     .pipe(postcss(shrink_opt))
