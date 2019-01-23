61520225df72267499a90c6c3a6204cd4bed9da2
J. Djimitry Riviere <tilithar@gmail.com>
2019-01-22 20:36:54 -0700

    New dev branch v2.3.x

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ A lightweight, open-source, mobile-first CSS framework for easy website designs.
 
 [![license](https://img.shields.io/badge/license-MIT-yellow.svg)](https://github.com/jdriviere/sparkle-css/blob/master/LICENSE)
 [![website](https://img.shields.io/badge/website-online-green.svg)](https://sparklecss.herokuapp.com)
-[![GitHub tag](https://img.shields.io/badge/version-2.2.0-blue.svg)](https://github.com/jdriviere/sparkle-css/releases/tag/v2.2.0)
-[![npm](https://img.shields.io/badge/npm-v2.2.0-red.svg)](https://www.npmjs.com/package/sparkle.css)
+[![GitHub tag](https://img.shields.io/badge/version-2.3.0-beta.1-blue.svg)](https://github.com/jdriviere/sparkle-css/releases/tag/v2.3.0-beta.1)
+[![npm](https://img.shields.io/badge/npm-v2.3.0-beta.1-red.svg)](https://www.npmjs.com/package/sparkle.css)
 [![jsDelivr](https://data.jsdelivr.com/v1/package/gh/jdriviere/sparkle-css/badge?style=rounded)](https://www.jsdelivr.com/package/gh/jdriviere/sparkle-css)
 [![David-DM](https://david-dm.org/jdriviere/sparkle-css.svg)](https://david-dm.org/jdriviere/sparkle-css)
 [![Travis](https://img.shields.io/travis/rust-lang/rust.svg)](https://travis-ci.org/jdriviere/sparkle-css)
@@ -47,7 +47,7 @@ The contribution guidelines can be found [here](https://github.com/jdriviere/spa
 ## Copyright and license
 MIT License
 
-Copyright (c) 2017-2018 J. Djimitry Rivière
+Copyright (c) 2017-2019 J. Djimitry Rivière
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 and associated documentation files (the "Software"), to deal in the Software without restriction,
--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,129 @@
+/**
+ * @name            Gulpfile for Gulp v4
+ * @author          J. D. Riviere
+ * @desc            This is the gulpfile.js that uses the Gulp v4 constalities = .
+ */
+
 /* ==================================== */
-/* PACKAGES
+/* MODULES
 /* ==================================== */
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
 const header = require('postcss-header');
 const cssnano = require('cssnano');
+const del = require('del');
 
+/* ==================================== */
+/* GLOBAL VARIABLES
+/* ==================================== */
 const pkg = require('./package.json');
+
+const paths = {
+  styles: {
+    src: 'src/sass/sparkle.sass',
+    dest: 'dist/css/'
+  }
+};
+
+const banner = `
+/*!
+  * @name         ${pkg.name}
+  * @author       ${pkg.author}
+  * @version      ${pkg.version} - released on XX/XX/2019
+  * @website      ${pkg.homepage}
+  * @license      ${pkg.license}
+  * Welcome and thank you for using Sparkle, a lightweight, open-source CSS framework
+  * to help you create simple, responsive website templates. It was developed by J. Djimitry Riviere
+  * in late 2016, and continues to be improved upon by its developer to bring more creative features.
+  * The major key to this framework lies within its ease of us to deliver superb results
+  * when creating a website.
+  * Thank you for using Sparkle! Happy Designing!
+  */
+`;
+
+const options = [
+  header({
+    header: banner
+  }),
+  autoprefixer({
+    browsers: [
+      'last 3 versions',
+      "IE >= 10"
+    ]
+  })
+];
+
+const shrinkOptions = [cssnano()];
 
 /* ==================================== */
 /* TASKS
 /* ==================================== */
-// RENDER CSS FILES
-gulp.task('sassify', function() {
-  const banner = `
-  /*!
-    * @name         ${pkg.name}
-    * @author       ${pkg.author}
-    * @version      ${pkg.version} - released on 11/14/2018
-    * @website      ${pkg.homepage}
-    * @license      ${pkg.license}
-    * Welcome and thank you for using Sparkle, a lightweight, open-source CSS framework
-    * to help you create simple, responsive website templates. It was developed by J. Djimitry Riviere
-    * in late 2016, and continues to be improved upon by its developer to bring more creative features.
-    * The major key to this framework lies within its ease of us to deliver superb results
-    * when creating a website.
-    * Thank you for using Sparkle! Happy Designing!
-    */
-  `;
-  const options = [
-    header({
-      header: banner
-    }),
-    autoprefixer({
-      browsers: ['last 3 versions', "IE >= 10"]
-    })
-  ];
-  const shrink_opt = [cssnano()];
+//--- Clean ---//
+const clean = () => {
+  console.log('Deleting files in \'dist\' folder...\n');
+  return del(['dist/css/sparkle.*']);
+}; // End of Clean
 
-  return gulp.src('src/sass/sparkle.sass')
-    .pipe(sass({
-        outputStyle: 'expanded'
-      })
-      .on('error', sass.logError)
-    )
+//--- Sassify ---//
+const sassify = () => {
+  console.log('Building expanded CSS file...\n');
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(postcss(options))
     .pipe(rename('sparkle.css'))
-    .pipe(gulp.dest('dist/css/'))
-    .pipe(postcss(shrink_opt))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.styles.dest));
+}; // End of Sassify
+
+//--- Minify ---//
+const minify = () => {
+  console.log('Building minified CSS files...\n');
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(postcss(options))
+    .pipe(postcss(shrinkOptions))
     .pipe(rename({
+      base: 'sparkle',
       suffix: '.min'
     }))
-    .pipe(gulp.dest('dist/css/'));
-});
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.styles.dest));
+}; // End of Minify
 
-// DEFAULT RUN
-gulp.task('default', ['sassify']);
+//--- Watch ---//
+const watch = () => {
+  console.log('Watching SASS files for changes...\n');
+  gulp.watch(paths.styles.src, sassify);
+}; // End of Watch
+
+/* ==================================== */
+/* SERIES AND PARALLELS
+/* ==================================== */
+const build = gulp.series(clean, gulp.parallel(sassify, minify));
+
+/* ==================================== */
+/* DESCRIPTIONS
+/* ==================================== */
+build.description = 'Builds the expanded CSS file in production mode.';
+clean.description = 'Deletes all files within the \'dist\' folder. A private command.';
+minify.description = 'Builds the minified CSS file in production mode.';
+sassify.description = 'Builds the CSS file in development mode.';
+watch.description = 'Watches all the CSS files (mainly the \'sparkle.sass\' file) for changes.';
+
+/* ==================================== */
+/* EXPORT TASKS
+/* ==================================== */
+exports.sassify = sassify;
+exports.minify = minify;
+exports.watch = watch;
+
+/* ==================================== */
+/* DEFAULT TASK RUNNER
+/* ==================================== */
+exports.default = build;
